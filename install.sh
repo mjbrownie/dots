3ec2f38ec0384a2e82b739472a3dc10ab4d7541d
Michael Brown <mjbrownie@gmail.com>
2017-03-18 21:42:01 +1100

    moved some dirs

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@ echo Moving old .vim.stuff
 mv ~/.vimrc ~/.vimrc.pre.plugged
 mv ~/.vim ~/.vim.pre.plugged
 
+ln -s $PWD/_vimrc ~/.vimrc
+ln -s $PWD/_vim ~/.vim
+
 echo Adding Plugged
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
