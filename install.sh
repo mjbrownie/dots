fe580d3f37d658f96f0bb3c9bad6543d7317c5cd
Michael Brown <mjbrownie@gmail.com>
2017-03-18 21:35:36 +1100

    added message

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+echo Moving old .vim.stuff
 mv ~/.vimrc ~/.vimrc.pre.plugged
 mv ~/.vim ~/.vim.pre.plugged
 
+echo Adding Plugged
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo done . Run :PlugInstall! 
